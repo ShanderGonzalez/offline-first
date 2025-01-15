d08e1cf3ae155c181e42919b06598cee081bd56e
ShanderGonzalez <kevin.gonzalez@gaheos.com>
2025-01-15 03:56:51 -0500

    Sincronizacion de dato inicial

--- lib/src/features/home/presentation/screens/home_screen.dart
+++ lib/src/features/home/presentation/screens/home_screen.dart
@@ -1,21 +1,44 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../../core/sync_service.dart';
 import '../blocs/res_partner_provider.dart';
 import '../widgets/custom_appbar.dart';
 import '../widgets/custom_button.dart';
 import 'created_widget.dart';
 import 'get_into_widget.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final SyncService syncService = SyncService();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      try {
+        await syncService.syncFromSupabase();
+        log('Sincronización completada exitosamente.');
+      } catch (e) {
+        log('Error durante la sincronización: $e');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final partnerProvider = Provider.of<ResPartnerProvider>(context);
     return Scaffold(
-      appBar: CustomAppBar(title: title),
+      appBar: CustomAppBar(title: widget.title),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
